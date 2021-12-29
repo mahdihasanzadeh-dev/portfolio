857e64d77b474f1f3f12954ac43655a3c6f04232
Mahdi Hasanzadeh <hasanzadehmahdi@gmail.com>
2021-12-29 11:39:25 +0330

    filter latest work

--- js/main.js
+++ js/main.js
@@ -16,3 +16,36 @@
         }
     })
 })();
+
+/*-------- portfolio filter and popup ---------*/
+(()=>{
+    const filterContainer = document.querySelector('.portfolio-filter')
+    const portfolioItemsContainer = document.querySelector('.portfolio-items')
+    const portfolioItems = document.querySelectorAll('.portfolio-item')
+    const popup = document.querySelector('.portfolio-popup')
+    const prevBtn = popup.querySelector('.pp-prev')
+    const nextBtn = popup.querySelector('.pp-next')
+    const closeBtn = popup.querySelector('.pp-close')
+    const projectDetailsContainer = popup.querySelector('.pp-details')
+    const projectDetailsBtn = popup.querySelector('.pp-project-details-btn')
+    let itemIndex,slideIndex,screenShots;
+    // filter portfolio items
+    filterContainer.addEventListener('click',(event)=>{
+     if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active")){
+         //deactive existing active 'filter-item'
+         filterContainer.querySelector('.active').classList.remove('active','shadow-outer')
+         // active new 'filter-item'
+         event.target.classList.add("shadow-outer","active")
+         const target = event.target.getAttribute('data-target');
+         portfolioItems.forEach((item)=>{
+             if(target===item.getAttribute('data-category') || target==='all'){
+                item.classList.remove('hide')
+                item.classList.add('show')
+             }else{
+                 item.classList.remove('show')
+                 item.classList.add('hide')
+             }
+         })
+     }
+    })
+})();
